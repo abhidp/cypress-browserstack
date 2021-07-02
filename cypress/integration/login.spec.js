e681e5937ca1ef8ee5818601432abafb0ebd20f7
abhiedapp <abhi@edapp.com>
2021-07-02 23:11:18 +1000

    login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,33 @@
+const email = 'prod1@gmail.com'
+const password = 'Test123456'
+
+describe('Login', () => {
+  it('Visit App', () => {
+    cy.visit('/')
+    cy.title().should('contain', 'Welcome to EdApp')
+  })
+
+  it('Click Sign In button', () => {
+    cy.contains('Sign in').click()
+    cy.url().should('include', '#login')
+  })
+
+  it('Enter Email', () => {
+    cy.get('[name="username"]').type(email)
+    cy.contains('Next').click()
+  })
+
+  it('Enter Password', () => {
+    cy.get('[name="password"]').clear().type(password)
+    cy.get('form').submit()
+    cy.url().should('include', '#home')
+  })
+
+  it('View landing page', () => {
+    cy.get('h1')
+      .should('be.visible')
+      .and('contain.text', 'Courses')
+      .and('contain.text', 'My Profile')
+      .and('contain.text', 'THIS WILL FAIL')
+  })
+})
