595688c374367dd2e5253b5bb85b2cc753195fd6
Abhi D <30851622+abhidp@users.noreply.github.com>
2021-07-10 23:12:48 +1000

    Browserstack (#3)
    * login
    
    * removed gitignored files
    
    * env test

---
+++ cypress/integration/production/fail2.spec.ts
@@ -0,0 +1,5 @@
+describe('Will Fail', () => {
+  it('2=3', () => {
+    expect(2).to.equal(3)
+  })
+})
---
+++ cypress/support/cy.run.js
@@ -0,0 +1,49 @@
+// runs Cypress tests using Cypress Node module API
+// https://on.cypress.io/module-api
+
+const cypress = require('cypress')
+
+let options = {
+  record: true,
+  config: {}
+}
+let counter = 3
+
+const runCypress = async (options) => {
+  await cypress
+    .run(options)
+    .then((result) => {
+      let exitCode = 0
+      let failedSpecs = []
+
+      if (result.runs)
+        if (result.runs.length > 0) {
+          for (let i = 0; i < result.runs.length; i++) {
+            for (let j = 0; j < result.runs[i].tests.length; j++) {
+              if (result.runs[i].tests[j].state === 'failed') {
+                failedSpecs.push(result.runs[i].spec.name)
+
+                exitCode++
+              }
+            }
+          }
+        }
+
+      options.config.testFiles = failedSpecs
+
+      console.log('options===', options)
+
+      counter--
+      if (counter) {
+        runCypress(options)
+      }
+
+      process.exitCode = exitCode
+    })
+    .catch((err) => {
+      console.error(err.message)
+      process.exit(1)
+    })
+}
+
+runCypress(options)
