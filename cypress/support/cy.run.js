e2b01113016b8a686115a9cc771461c30c89b1fe
abhiedapp <abhi@edapp.com>
2021-07-11 00:24:09 +1000

    chk duplicates

--- cypress/support/cy.run.js
+++ cypress/support/cy.run.js
@@ -2,48 +2,54 @@
 // https://on.cypress.io/module-api
 
 const cypress = require('cypress')
+const _ = require('lodash')
 
 let options = {
-  record: true,
+  quiet: true,
+  tag: 'retry-run',
   config: {}
 }
-let counter = 3
+let retryCount = 3
 
 const runCypress = async (options) => {
-  await cypress
-    .run(options)
-    .then((result) => {
-      let exitCode = 0
-      let failedSpecs = []
+  const cyRun = async (options) => {
+    await cypress
+      .run(options)
+      .then((result) => {
+        let exitCode = 0
+        let failedSpecs = []
 
-      if (result.runs)
-        if (result.runs.length > 0) {
-          for (let i = 0; i < result.runs.length; i++) {
-            for (let j = 0; j < result.runs[i].tests.length; j++) {
-              if (result.runs[i].tests[j].state === 'failed') {
-                failedSpecs.push(result.runs[i].spec.name)
+        if (result.runs)
+          if (result.runs.length > 0) {
+            for (let i = 0; i < result.runs.length; i++) {
+              for (let j = 0; j < result.runs[i].tests.length; j++) {
+                if (result.runs[i].tests[j].state === 'failed') {
+                  failedSpecs.push(result.runs[i].spec.name)
 
-                exitCode++
+                  exitCode++
+                }
               }
             }
           }
+
+        options.config.testFiles = failedSpecs
+
+        retryCount--
+        console.log('retry===', retryCount, options)
+        if (retryCount) {
+          // _.delay().runCypress(options)
+          _.delay(runCypress, 50000, options)
         }
 
-      options.config.testFiles = failedSpecs
-
-      console.log('options===', options)
-
-      counter--
-      if (counter) {
-        runCypress(options)
-      }
-
-      process.exitCode = exitCode
-    })
-    .catch((err) => {
-      console.error(err.message)
-      process.exit(1)
-    })
+        process.exitCode = exitCode
+        // }
+      })
+      .catch((err) => {
+        console.error(err.message)
+        process.exit(1)
+      })
+  }
+  cyRun(options)
 }
 
 runCypress(options)
